23977fa4ee6270edb26f1ed10d0dcd694062caff
laayounii <meedaziz001@gmail.com>
2024-01-31 11:48:52 +0100

    two digits

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(0, 10):
+    for j in range(1, 10):
+        if i != j and i < j and (str(i) + str(j)) != "89":
+            print("{},".format(str(i) + str(j)), end=" ")
+        if (str(i) + str(j)) == "89":
+            print("{}".format(str(i)+str(j)))
