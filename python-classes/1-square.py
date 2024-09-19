043af8a1c5ec9df982d0bb65548036c51d7a78ea
laayounii <meedaziz001@gmail.com>
2024-09-19 17:27:05 +0100

    Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square by size"""
+
+
+class Square():
+    """ create a square"""
+    def __init__(self, size):
+        """ inisalise the size of the square"""
+        self.__size = size
