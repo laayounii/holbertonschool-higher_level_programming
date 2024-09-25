e33896de429ad92a3301a71c44cc4c8897f7483e
laayounii <meedaziz001@gmail.com>
2024-09-25 15:56:04 +0100

    Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+module base geometry
+"""
+
+
+class BaseGeometry:
+    """create class geometry with Public instance method: area"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
+    
